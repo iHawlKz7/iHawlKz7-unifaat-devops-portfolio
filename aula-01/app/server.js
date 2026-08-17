ecd8c68eccbf9a0eb0ac621e23ce8344eafc926e
Caroline Lejne Geli <carolgeli34@gmail.com>
2026-08-17 20:37:42 -0300

    feat: atualiza mensagem para v2

--- aula-01/app/server.js
+++ aula-01/app/server.js
@@ -6,9 +6,9 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json({
-    servico: 'DevOps Portfolio API',
-    aluno: 'SEU NOME AQUI',
-    ra: 'SEU RA AQUI',
+    servico: 'DevOps Portfolio API v2',
+    aluno: 'Caroline Lejne Geli',
+    ra: '6325016',
     aula: '01 - Fundamentos de Git e Docker',
     status: 'online',
     timestamp: new Date().toISOString()
